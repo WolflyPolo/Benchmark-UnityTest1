6e49a1f2babc3e0bbb64cf3dcc055de306174c80
Samuel Polo Peña <sammy-halo@hotmail.com>
2017-09-03 22:16:35 -0500

    First commit

---
+++ Assets/AvispaScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvispaScript : MonoBehaviour {
+
+	// Use this for initialization
+	//Script generador de cubos con física. 
+	public GameObject cuboPrefab;
+	private float posicionRandom;
+	private float segReset = 0f;
+	public float reset = 0;
+	private int contador_cubos = 0;
+	public Text cubos_text;
+	public int fpsRate;
+	public Text fpsText;
+
+	void Start () {
+		InvokeRepeating ("Spawnear", reset, reset);
+	}
+		
+	// Update is called once per frame
+
+	void Update () {
+		fpsRate = (int)(1f / Time.unscaledDeltaTime);
+		cubos_text.text = "C U B E S : " + contador_cubos;
+		fpsText.text = "F P S : " + fpsRate;
+	}
+		
+	void Spawnear() {
+		posicionRandom = Random.Range (-24, 24);
+		Instantiate (cuboPrefab, new Vector2(posicionRandom, 10f), cuboPrefab.transform.rotation);
+		contador_cubos++;
+	}
+
+
+}
---
+++ Assets/SaltamontesScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaltamontesScript : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector3 mousePosition, targetPosition;
+	public GameObject cuboPrefab;
+	private float distance = 10f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			mousePosition = Input.mousePosition;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);        
+			targetPosition=Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x,mousePosition.y,distance));
+			cuboPrefab.transform.position = targetPosition;
+			Debug.Log ("mouse position: " + "x: "+Input.mousePosition.x +"y: "+Input.mousePosition.y);
+			Debug.Log ("entro a fire1");
+				Instantiate (cuboPrefab, transform.position, transform.rotation);
+		}
+	}
+}
